01b78c5f32b3d103cc4d6f264fff9a0c249b8774
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-06 21:28:03 -0500

    Reajustada la clase factura, crea el carrito y genera valor total

--- Factura.js
+++ Factura.js
@@ -1,28 +1,61 @@
+import Electrodomesticos from "./Electrodomesticos.js";
+import Televisor from "./Televisor.js";
+import Nevera from "./Nevera.js";
+
 var precioTotal = 0;
+var carrito = [];
 
 export default function crearFactura(inventario){
-
-   
-    var precioTvs = 0;
-    var precioNeveras = 0;
-    var precioElectrodomesticos = 0;
-
-    calcularPrecioElectrodomestico(inventario, 5, "televisor", "B", "importado");
+    
+    carritoDeCompras();
+    calcularPrecioElectrodomestico(inventario, 0, "televisor", "B", "importado");
     console.log(precioTotal);
+    console.log(inventario);
 
 }
 
 function calcularPrecioElectrodomestico(inventario, cantidad, tipo, consumo, nacionalidad){
-    let contador = 0;
+    
+    carrito.forEach(element => {
+        precioTotal += element.getPrecio();
+    });
 
-    for(let i = 0; i<inventario.length; i++){
-        if(cantidad===contador){break;}
+}
 
-        if(inventario[i].getTipo() === tipo && inventario[i].getConsumo() === consumo && inventario[i].getNacionalidad() === nacionalidad){
-            precioTotal += inventario[i-contador].getPrecio();
-            inventario.splice(i-contador,1);
-            contador++;
-        }
+function anadirElectrodomesticosCarrito(numero, consumo, nacionalidad){
+
+    for(var i = 0; i<numero; i++){
+        var electrodomestico = new Electrodomesticos(consumo, nacionalidad);
+        electrodomestico.calcularPrecio();
+        carrito.push(electrodomestico);
+    }
+    
+}
+
+function anadirTelevisoresCarrito(numero, consumo, nacionalidad, pulgadas, tdt){
+
+    for(var i = 0; i<numero; i++){
+        var televisor = new Televisor(consumo, nacionalidad, pulgadas, tdt);
+        televisor.calcularPrecio();
+        carrito.push(televisor);
+    }
+        
+}
+
+function anadirNeverasCarrito(numero, consumo, nacionalidad, capacidad){
+
+    for(var i = 0; i<numero; i++){
+        var nevera = new Nevera(consumo, nacionalidad, capacidad);
+        nevera.calcularPrecio();
+        carrito.push(nevera);
     }
 
+}
+
+
+
+function carritoDeCompras(){
+    anadirTelevisoresCarrito(1, "B", "importado", 37, false);
+    anadirNeverasCarrito(2, "C", "importado", 120);
+    anadirElectrodomesticosCarrito(5, "B", "nacional");
 }
--- Inventario.js
+++ Inventario.js
@@ -5,33 +5,26 @@ import Televisor from "./Televisor.js";
 var inventario = [];
 
 export function crearElectrodomesticos(numero, consumo, nacionalidad){
-    var electrodomesticos = [];
 
     for(var i = 0; i<numero; i++){
         var electrodomestico = new Electrodomesticos(consumo, nacionalidad);
         electrodomestico.calcularPrecio();
         inventario.push(electrodomestico);
     }
-
-    return electrodomesticos;
-        
+    
 }
 
 export function crearTelevisores(numero, consumo, nacionalidad, pulgadas, tdt){
-    var televisores = [];
 
     for(var i = 0; i<numero; i++){
         var televisor = new Televisor(consumo, nacionalidad, pulgadas, tdt);
         televisor.calcularPrecio();
         inventario.push(televisor);
     }
-
-    return televisores;
         
 }
 
 export function crearNeveras(numero, consumo, nacionalidad, capacidad){
-    var neveras = [];
 
     for(var i = 0; i<numero; i++){
         var nevera = new Nevera(consumo, nacionalidad, capacidad);
@@ -39,15 +32,14 @@ export function crearNeveras(numero, consumo, nacionalidad, capacidad){
         inventario.push(nevera);
     }
 
-    return neveras;
 }
 
 export default function crearInventarioDefault(){
-    
+
     crearElectrodomesticos(5, "B", "nacional");
     crearNeveras(10, "A", "nacional", 140);
     crearTelevisores(7, "C", "importado", 40, true);
-    crearTelevisores(13, "B", "importado", 30, true);
+    crearTelevisores(13, "B", "importado", 37, false);
     crearTelevisores(3, "A", "nacional", 47, false);
     crearElectrodomesticos(8, "A", "importado");
     crearNeveras(12, "C", "importado", 120);
