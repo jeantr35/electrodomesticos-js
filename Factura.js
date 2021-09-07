797e8745e8170a1188abbc502e6204e9b87566e4
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-06 23:50:47 -0500

    Funciona el carrito bien, excepto el tv

--- Factura.js
+++ Factura.js
@@ -3,62 +3,19 @@ import Televisor from "./Televisor.js";
 import Nevera from "./Nevera.js";
 
 var precioTotal = 0;
-var carrito = [];
 
-export default function crearFactura(inventario){
+export default function crearFactura(inventario, carrito){
     
-    calcularPrecioElectrodomestico();
-    console.log(precioTotal);
+    calcularPrecioElectrodomestico(carrito);
     console.log(inventario);
+    return precioTotal;
 
 }
 
-function calcularPrecioElectrodomestico(){
+function calcularPrecioElectrodomestico(carrito){
     
     carrito.forEach(element => {
         precioTotal += element.getPrecio();
     });
 
-}
-
-function anadirElectrodomesticosCarrito(numero, consumo, nacionalidad){
-
-    for(var i = 0; i<numero; i++){
-        var electrodomestico = new Electrodomesticos(consumo, nacionalidad);
-        electrodomestico.calcularPrecio();
-        carrito.push(electrodomestico);
-    }
-    
-}
-
-function anadirTelevisoresCarrito(numero, consumo, nacionalidad, pulgadas, tdt){
-
-    for(var i = 0; i<numero; i++){
-        var televisor = new Televisor(consumo, nacionalidad, pulgadas, tdt);
-        televisor.calcularPrecio();
-        carrito.push(televisor);
-    }
-        
-}
-
-function anadirNeverasCarrito(numero, consumo, nacionalidad, capacidad){
-
-    for(var i = 0; i<numero; i++){
-        var nevera = new Nevera(consumo, nacionalidad, capacidad);
-        nevera.calcularPrecio();
-        carrito.push(nevera);
-    }
-
-}
-
-function carritoDeCompras(tipo, cantidad, consumo, nacionalidad, parametro1, parametro2){
-    if (tipo === "televisor") {
-        anadirTelevisoresCarrito(cantidad, consumo, nacionalidad, parametro1, parametro2);    
-    }
-    if (tipo === "nevera") {
-        anadirNeverasCarrito(cantidad, consumo, nacionalidad, parametro1);    
-    }
-    if (tipo === "electrodomestico") {
-        anadirElectrodomesticosCarrito(cantidad, consumo, nacionalidad);    
-    }
 }
--- main.js
+++ main.js
@@ -4,11 +4,88 @@ import crearInventarioDefault from "./Inventario.js";
 import Nevera from "./Nevera.js";
 import Televisor from "./Televisor.js";
 
-var respuesta = prompt("Bienvendo al sistema de facturacion, desea crear un inventario con la informacion dada? \n1) Si\n2) No\n");
+var respuesta = prompt("Bienvendo al sistema de facturacion, desea crear un inventario default? \n1) Si\n2) No\n");
+var cantidad;
+var tipo;
+var consumo
+var nacionalidad;
+var tamano;
+var esTdt;
+var capacidad;
+var carrito = [];
 
 if(respuesta === "1"){
    var inventario = crearInventarioDefault();
 }
 
-crearFactura(inventario);
+while(respuesta !== "3"){
+
+    respuesta = prompt("Seleccione una opcion:\n1) Anadir items a la cesta\n 2) Generar factura\n 3)Salir");
+
+    switch (respuesta) {
+        case "1":
+            tipo = prompt("Que desea anadir? (televisor, nevera, electrodomestico)");
+
+            switch (tipo) {
+                case "electrodomestico":
+                    consumo = prompt("Ingrese el consumo (A, B, C)");
+                    nacionalidad = prompt("Ingrese la nacionalidad (nacional, importado)");
+                    for (let i = 0; i < inventario.length; i++) {
+                        if (tipo == inventario[i].getTipo() && consumo == inventario[i].getConsumo() && nacionalidad == inventario[i].getNacionalidad()) {
+                            carrito.push(inventario[i]);
+                            inventario.splice(i,1);
+                            break
+                        }
+                    }
+                    break;
+
+                case "nevera":
+                    consumo = prompt("Ingrese el consumo (A, B, C)");
+                    nacionalidad = prompt("Ingrese la nacionalidad (nacional, importado)");
+                    capacidad = parseInt(prompt("Ingrese la capacidad: "),10);
+                    for (let i = 0; i < inventario.length; i++) {
+                    if (tipo == inventario[i].getTipo() && consumo == inventario[i].getConsumo() && nacionalidad == inventario[i].getNacionalidad() && capacidad == inventario[i].getCapacidad()){
+                        carrito.push(inventario[i]);
+                        inventario.splice(i,1);
+                        break;
+                    }
+                    }
+                    break;
+
+                case "televisor":
+                    consumo = prompt("Ingrese el consumo (A, B, C)");
+                    nacionalidad = prompt("Ingrese la nacionalidad (nacional, importado)");
+                    tamano = parseInt(prompt("Ingrese las pulgadas: "),10);
+                    esTdt = prompt("Es tdt? (si, no)");
+                    if (esTdt === "si") {
+                        esTdt = true;
+                    }
+                    else{esTdt = false;}
+                    for (let i = 0; i < inventario.length; i++) {
+                    if (tipo == inventario[i].getTipo() && consumo == inventario[i].getConsumo() && nacionalidad == inventario[i].getNacionalidad()){
+                        carrito.push(inventario[i]);
+                        inventario.splice(i,1);
+                        console.log(tamano);
+                        break;
+                    }
+                    }
+                    break;
+            
+                default:
+                    break;
+            }
+            break;
+        
+        case "2":
+            prompt("El costo total es de: " + crearFactura(inventario, carrito) + " gracias por su compra");
+            carrito = [];
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+
 
